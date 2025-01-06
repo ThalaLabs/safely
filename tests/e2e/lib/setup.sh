54158a8fd03440c83ff9870a8092f4bea5b1cf9c
SamuelQZQ <qianzheqi@foxmail.com>
2025-01-07 02:15:27 +0800

    debug

--- tests/e2e/lib/setup.sh
+++ tests/e2e/lib/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x  # Add this to see all commands being executed
 
 # Colors for output
 export GREEN='\033[0;32m'
--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e # Exit on error
+# set -e # Exit on error
+set -x  # Add this to see all commands being executed
 
 # Run all test files
 for test_file in $(find "$(dirname "$0")/commands" -name "*.sh"); do
