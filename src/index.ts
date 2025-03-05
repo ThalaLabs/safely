eea44653d49b24413aecdae4bd499e57ba123489
0xbe1 <0xbetrue@gmail.com>
2025-03-05 22:01:08 +0800

    fix --version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const packageJson = JSON.parse(readFileSync(join(__dirname, '..', 'package.json'), 'utf-8'));
 
 import { registerAddressesCommand } from './commands/addresses.js';
 import { registerDecodeCommand } from './commands/decode.js';
@@ -16,7 +23,7 @@ import { registerDefaultCommand } from './commands/defaults.js';
 
 const program = new Command();
 
-program.name('safely').description('CLI tool for multisig management').version('0.0.1');
+program.name('safely').description('CLI tool for multisig management').version(packageJson.version);
 
 // important
 registerAccountCommand(program);
